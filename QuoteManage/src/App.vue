dd56ded53cc6890c27db1b6fa2104202346e47b7
ittus <vuminhthang.cm@gmail.com>
2017-02-27 08:59:34 +0800

    Quote Manager

---
+++ QuoteManage/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+    	<app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    	<app-new-quote @quoteAdd="newQuote"></app-new-quote>
+    	<app-quote-grid :quotes="quotes" @quoteDelete="deleteQuote"></app-quote-grid>
+    	<div class="row">
+    		<div class="col-sm-12 text-center">
+    			<div class="alert alert-info">Info: Click on a quote to delete it!</div>
+    		</div>
+    	</div>
+    </div>
+</template>
+
+<script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+	import NewQuote from './components/NewQuote.vue';
+	import Header from './components/Header.vue';
+    export default {
+        data: function() {
+        	return {
+        		maxQuotes: 10,
+        		quotes: [
+        			'Just a quote to see st'
+        		]
+        	}
+        },
+        components: {
+        	'app-quote-grid': QuoteGrid,
+        	'app-new-quote': NewQuote,
+        	'app-header': Header
+        },
+        methods: {
+        	newQuote(quote) {
+        		if (this.quotes.length > this.maxQuotes) {
+        			return alert("Please delete quote first!");
+        		}
+        		this.quotes.push(quote);
+        	}, 
+        	deleteQuote(idx) {
+        		this.quotes.splice(idx, 1);
+        	}
+        }
+    }
+</script>
+
+<style>
+</style>
