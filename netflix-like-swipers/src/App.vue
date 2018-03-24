339c778f806c8bfce4b290e7afcf2a1c70eee50b
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-03-24 17:34:22 +0900

    Netflix like swippers
    Add build script

---
+++ netflix-like-swipers/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>{{ swiperType }}</h1>
+    <HomePage/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage'
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  },
+  data() {
+    return {
+      swiperType : 'Awesome swipper'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+h1{
+  color: #ffffff;
+}
---
+++ netflix-like-swipers/src/components/Movie.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="movie"
+     v-bind:style="{ backgroundImage: 'url(' + image + ')' }">
+  <div class="content">
+    <h1 class="title">{{ title }}</h1>
+    <p class="description">{{ description }}</p>
+    <p class="duration">{{ duration }}</p>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: "movie",
+  props: [
+    'image',
+    'title',
+    'description',
+    'duration'
+  ],
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.movie{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+  text-align: left;
+  width : 100%;
+  height : 800px;
+  background-color: rgba(255, 255, 255, 0.7);
+  background-repeat: no-repeat;
+  background-blend-mode: overlay;
+  background-size: cover;
+  background-position: center;
+}
+.content {
+  padding: 10px;
+}
+</style>
