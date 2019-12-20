0e0f626614b6d29018536df220d1dfcf29eeef24
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-20 15:02:37 +0900

    Add marker

--- typescript/maps/src/CustomMap.ts
+++ typescript/maps/src/CustomMap.ts
@@ -1,3 +1,10 @@
+export interface Mappable {
+  location: {
+    lat: number
+    lng: number
+  }
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map
 
@@ -8,6 +15,13 @@ export class CustomMap {
         lat: 0,
         lng: 0
       }
+    })
+  }
+
+  addMarker(user: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: user.location
     })
   }
 }
