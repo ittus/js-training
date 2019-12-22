9c700fe4cbb6ff4f1d9f93723e21f029eca10f73
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-22 21:15:32 +0900

    Add string sort

--- typescript/sort/src/index.ts
+++ typescript/sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter'
 import { NumberCollection } from './NumberCollection'
+import { CharactersCollection } from './CharactersCollection'
 
-const numbersCollection = new NumberCollection([10, 3, -5, 0])
-const sorter = new Sorter(numbersCollection)
+// const numbersCollection = new NumberCollection([10, 3, -5, 0])
+// const sorter = new Sorter(numbersCollection)
+// sorter.sort()
+// console.log(numbersCollection.data)
+
+const charactersCollection = new CharactersCollection('Xaayb')
+const sorter = new Sorter(charactersCollection)
 sorter.sort()
-console.log(numbersCollection.data)
+console.log(charactersCollection.data)
