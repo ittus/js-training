5472a135255b4aca018245e95ea44a7efc3f19b9
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-22 22:04:54 +0900

    Add LinkList sort

--- typescript/sort/src/index.ts
+++ typescript/sort/src/index.ts
@@ -1,3 +1,4 @@
+import { LinkedList } from './LinkedList'
 import { Sorter } from './Sorter'
 import { NumberCollection } from './NumberCollection'
 import { CharactersCollection } from './CharactersCollection'
@@ -7,7 +8,18 @@ import { CharactersCollection } from './CharactersCollection'
 // sorter.sort()
 // console.log(numbersCollection.data)
 
-const charactersCollection = new CharactersCollection('Xaayb')
-const sorter = new Sorter(charactersCollection)
+// const charactersCollection = new CharactersCollection('Xaayb')
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
+const sorter = new Sorter(linkedList)
 sorter.sort()
-console.log(charactersCollection.data)
+
+linkedList.print()
