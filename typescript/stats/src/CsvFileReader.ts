883fc7d1a1996c4d5569c43abbf481d1de0dcd5b
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-23 14:30:05 +0900

    Use generic

--- typescript/stats/src/CsvFileReader.ts
+++ typescript/stats/src/CsvFileReader.ts
@@ -1,12 +1,11 @@
 import fs from 'fs'
-import { dateStringToDate } from './utils'
-import { MatchResult } from './MatchResult'
 
-type MatchData = [Date, string, string, number, number, MatchResult, string]
+export abstract class CsvFileReader<T> {
+  data: T[] = []
 
-export class CsvFileReader {
-  data: MatchData[] = []
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T
+
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
@@ -16,18 +15,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',')
       })
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-          ]
-        }
-      )
+      .map(this.mapRow)
   }
 }
