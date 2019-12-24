c822d2306ca969ec70232b47d2bc4299954fff83
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-24 13:59:51 +0900

    Use composition

--- typescript/stats/src/index.ts
+++ typescript/stats/src/index.ts
@@ -1,11 +1,13 @@
 import { MatchResult } from './MatchResult'
 import { MatchReader } from './MatchReader'
+import { CsvFileReader } from './CsvFileReader'
 
-const reader = new MatchReader('football.csv')
-reader.read()
+const csvFileReader = new CsvFileReader('football.csv')
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
 let manUnitedWins = 0
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
--- typescript/stats/src/inheritance/CsvFileReader.ts
+++ typescript/stats/src/inheritance/CsvFileReader.ts
@@ -1,9 +1,10 @@
 import fs from 'fs'
 
-export class CsvFileReader {
-  data: string[][] = []
+export abstract class CsvFileReader<T> {
+  data: T[] = []
 
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T
 
   read(): void {
     this.data = fs
@@ -14,5 +15,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',')
       })
+      .map(this.mapRow)
   }
 }
