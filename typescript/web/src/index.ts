74c98a6515e570a1850f4d52ee607f4d17e71012
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 10:46:14 +0900

    Extract Event

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -5,5 +5,5 @@ user.fetch()
 user.set({ name: 'Thang Vu', age: 30 })
 user.save()
 
-const user2 = new User({name: 'John', age: 8888})
-user2.save()
+// const user2 = new User({name: 'John', age: 8888})
+// user2.save()
