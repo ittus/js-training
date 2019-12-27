93cb5407a8656dc8615986808615e8089a3589c6
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 13:32:17 +0900

    Add Attributes clas

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,9 +1,4 @@
 import { User } from './models/User';
 
-const user = new User({id: 1})
-user.fetch()
-user.set({ name: 'Thang Vu', age: 30 })
-user.save()
-
-// const user2 = new User({name: 'John', age: 8888})
-// user2.save()
+const user = new User({name: 'Java', age: 27})
+user.sync.save()
