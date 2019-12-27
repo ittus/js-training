f73580ff6247847ec1e4aeb58fe129958a7ab5c9
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 17:38:54 +0900

    Create Nested view

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,11 +1,11 @@
-import { UserForm } from './views/UserForm';
 import { User } from './models/User';
+import UserEdit from './views/UserEdit';
 
 const user = User.buildUser({ name: 'NAME', age: 20})
 const root = document.getElementById('root')
 if (root) {
-  const userForm = new UserForm(root, user)
-  userForm.render()
+  const userEdit = new UserEdit(root, user)
+  userEdit.render()
 } else {
   throw new Error('Root element not found')
 }
