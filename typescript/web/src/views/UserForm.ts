276667df23a3a680355767e8a9bebaae92a21305
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 16:19:50 +0900

    Enable strict mode

--- typescript/web/src/views/UserForm.ts
+++ typescript/web/src/views/UserForm.ts
@@ -25,8 +25,10 @@ export class UserForm {
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input')
-    const name = input.value
-    this.model.set({ name })
+    if (input) {
+      const name = input.value
+      this.model.set({ name })
+    }
   }
 
   template(): string {
