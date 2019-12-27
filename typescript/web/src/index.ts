9048cc64df7d0091ef3eafcda35d4a14a7c8ec62
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 13:55:27 +0900

    Delegate event

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,4 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({name: 'Java', age: 27})
-user.sync.save()
+const user = new User({ id: 1, name: 'amazing', age: 22})
+// console.log(user.get('name'))
+user.on('save', () => {
+  console.log('user was saved', user)
+})
+
+user.save()
--- typescript/web/src/models/User.ts
+++ typescript/web/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios'
 
 export interface UserProps {
   id?: number
@@ -29,5 +30,31 @@ export class User {
 
   get get() {
     return this.attributes.get
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update)
+    this.events.trigger('change')
+  }
+
+  fetch(): void {
+    const id = this.get('id')
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id')
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data)
+    })
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save')
+      })
+      .catch(() => {
+        this.trigger('error')
+      })
   }
 }
