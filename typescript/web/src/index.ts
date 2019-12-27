63f25e419abbc983b39754f6e6b446441aba3be9
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 15:21:56 +0900

    Add UserForm

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,7 +1,3 @@
-import { User } from './models/User';
-
-const collection = User.buildUserCollection()
-collection.on('change', () => {
-  console.log(collection)
-})
-collection.fetch()
+import { UserForm } from './views/UserForm';
+const userForm = new UserForm(document.getElementById('root'))
+userForm.render()
