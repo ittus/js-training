4abfcf9d897a60e667ca55e84438e02703ebd7bf
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 10:30:48 +0900

    Add json server

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,7 +1,9 @@
-import { User } from './models/User'
-const user = new User({ name: 'Thang', age: 27 })
-user.on('change', () => {
-  console.log('change')
-})
-console.log(user)
-user.trigger('change')
+import { User } from './models/User';
+
+const user = new User({id: 1})
+user.fetch()
+user.set({ name: 'Thang Vu', age: 30 })
+user.save()
+
+const user2 = new User({name: 'John', age: 8888})
+user2.save()
--- typescript/web/src/models/User.ts
+++ typescript/web/src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios'
+
 interface UserProps {
+  id?: number
   name?: string
   age?: number
 }
@@ -30,5 +33,21 @@ export class User {
       return
     }
     handlers.forEach(callback => callback())
+  }
+
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data)
+      })
+  }
+
+  save(): void {
+    const id = this.get('id')
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data)
+    } else {
+      axios.post(`http://localhost:3000/users`, this.data)
+    }
   }
 }
