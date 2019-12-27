84bf3abfb12bc5bf4fbc87c7ae89c9d7ff712062
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-27 17:54:08 +0900

    Add User Collection View

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,11 +1,25 @@
-import { User } from './models/User';
+import { User, UserProps } from './models/User';
 import UserEdit from './views/UserEdit';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'NAME', age: 20})
-const root = document.getElementById('root')
-if (root) {
-  const userEdit = new UserEdit(root, user)
-  userEdit.render()
-} else {
-  throw new Error('Root element not found')
-}
+// const user = User.buildUser({ name: 'NAME', age: 20})
+// const root = document.getElementById('root')
+// if (root) {
+//   const userEdit = new UserEdit(root, user)
+//   userEdit.render()
+// } else {
+//   throw new Error('Root element not found')
+// }
+
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => User.buildUser(json))
+users.on('change', () => {
+  const root = document.getElementById('root')
+  if (root) {
+    const userList = new UserList(root, users)
+    userList.render()
+  }
+})
+users.fetch()
+
