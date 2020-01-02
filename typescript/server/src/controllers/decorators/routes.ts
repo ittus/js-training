3142562e8b8b130d96bd6b172a1e449bef0a92b0
Vu Minh Thang <vuminhthang.cm@gmail.com>
2020-01-02 21:48:56 +0900

    Start refactoring using decorators

---
+++ typescript/server/src/controllers/decorators/routes.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata'
+
+function routeBinder(method: string) {
+  return function get(path: string) {
+    return function(target: any, key: any, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key)
+      Reflect.defineMetadata('method', method, target, key)
+    }
+  }
+}
+
+export const get = routeBinder('get')
+export const put = routeBinder('put')
+export const post = routeBinder('post')
+export const del = routeBinder('del')
+export const patch = routeBinder('patch')
