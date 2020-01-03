7dcd5636a312d245feb22639b48a55b7dbcf69a6
Vu Minh Thang <vuminhthang.cm@gmail.com>
2020-01-03 23:28:12 +0900

    Response to attribute change
    Improve css

--- web-components/udemy-web-components-stenciljs/basic/tooltip.js
+++ web-components/udemy-web-components-stenciljs/basic/tooltip.js
@@ -1,20 +1,32 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super()
-    this._tooltipContainer;
+    this._tooltipIcon;
+    this._tooltipVisible = false
     this._tooltipText = "Some dummy tooltip text."
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.innerHTML = `
       <style>
         div {
+          font-weight: normal;
           background-color: black;
           color: white;
           position: absolute;
+          top: 1.5rem;
+          left: 0.75rem;
           z-index: 10;
+          padding: 0.15rem;
+          border-radius: 3px;
+          box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
+        }
+
+        :host {
+          position: relative;
         }
 
         :host(.important) {
           background: var(--color-primary, #ccc);
+          padding: 0.15rem;
         }
 
         :host-context(p) {
@@ -46,22 +58,51 @@ class Tooltip extends HTMLElement {
     if (this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text')
     }
-    const tooltipIcon = this.shadowRoot.querySelector('span')
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
-    this.shadowRoot.appendChild(tooltipIcon)
-    this.style.position = 'relative'
+    this._tooltipIcon = this.shadowRoot.querySelector('span')
+    this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+    this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue
+    }
+  }
+
+  static get observedAttributes() {
+    return ['text']
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip)
+    this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip)
+  }
+
+  _render() {
+    let tooltipContainer = this.shadowRoot.querySelector('div');
+    if (this._tooltipVisible) {
+      tooltipContainer = document.createElement('div')
+      tooltipContainer.textContent = this._tooltipText
+
+      this.shadowRoot.appendChild(tooltipContainer)
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer)
+      }
+    }
   }
 
   _showTooltip() {
-    this._tooltipContainer = document.createElement('div')
-    this._tooltipContainer.textContent = this._tooltipText
-
-    this.shadowRoot.appendChild(this._tooltipContainer)
+    this._tooltipVisible = true
+    this._render()
   }
 
   _hideTooltip() {
-    this.shadowRoot.removeChild(this._tooltipContainer)
+    this._tooltipVisible = false
+    this._render()
   }
 }
 
