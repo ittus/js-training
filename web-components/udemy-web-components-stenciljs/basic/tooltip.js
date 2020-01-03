532d247fc71db1f85c299101cc7320eaf9d4a1e9
Vu Minh Thang <vuminhthang.cm@gmail.com>
2020-01-03 21:38:26 +0900

    Add style

--- web-components/udemy-web-components-stenciljs/basic/tooltip.js
+++ web-components/udemy-web-components-stenciljs/basic/tooltip.js
@@ -12,11 +12,29 @@ class Tooltip extends HTMLElement {
           position: absolute;
           z-index: 10;
         }
+
+        :host {
+          background: lightgrey;
+        }
+
+        .highlight {
+          background-color: red;
+        }
+
+        ::slotted(.highlight) {
+          border-bottom: 1px dotted red;
+        }
+
+        .icon {
+          background: black;
+          color: white;
+          padding: 0.15rem 0.5rem;
+          text-align: center;
+          border-radius: 50%;
+        }
       </style>
       <slot>Some default</slot>
-      <span>
-        (?)
-      </span>
+      <span class="icon">?</span>
     `
   }
 
