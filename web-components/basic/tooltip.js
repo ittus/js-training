8050b453ac885782886e815b7d2260022ae74a2a
Vu Minh Thang <vuminhthang.cm@gmail.com>
2020-01-03 19:17:08 +0900

    Add event listener

--- web-components/basic/tooltip.js
+++ web-components/basic/tooltip.js
@@ -1,12 +1,35 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super()
+    this._tooltipContainer;
+    this._tooltipText = "Some dummy tooltip text."
   }
 
   connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text')
+    }
     const tooltipIcon = document.createElement('span')
     tooltipIcon.textContent = ' (?)'
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
     this.appendChild(tooltipIcon)
+    this.style.position = 'relative'
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div')
+    this._tooltipContainer.textContent = this._tooltipText
+    this._tooltipContainer.style.backgroundColor = 'black'
+    this._tooltipContainer.style.color = 'white'
+    this._tooltipContainer.style.position = 'absolute'
+    this._tooltipContainer.style.zIndex = '10'
+
+    this.appendChild(this._tooltipContainer)
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer)
   }
 }
 
