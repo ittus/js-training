7a65486c3db5be3641cc0d0108c881a3d0082d10
Vu Minh Thang <vuminhthang.cm@gmail.com>
2020-01-03 16:52:51 +0900

    Create simple web component

---
+++ web-components/basic/tooltip.js
@@ -0,0 +1,13 @@
+class Tooltip extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    const tooltipIcon = document.createElement('span')
+    tooltipIcon.textContent = ' (?)'
+    this.appendChild(tooltipIcon)
+  }
+}
+
+customElements.define('uc-tooltip', Tooltip)
