37125fbe3e1fac691ace05fc9f1922942ec87cf0
Vu Minh Thang <vuminhthang.cm@gmail.com>
2020-01-04 10:27:36 +0900

    Add open and close state of sidebar

--- web-components/udemy-web-components-stenciljs/web-component-stencil/src/components/side-drawer/side-drawer.tsx
+++ web-components/udemy-web-components-stenciljs/web-component-stencil/src/components/side-drawer/side-drawer.tsx
@@ -7,20 +7,23 @@ import { h, Component, Prop } from '@stencil/core'
 })
 export class SideDrawer {
   @Prop({ reflect: true }) title: string
-  @Prop() open: boolean
+  @Prop({ reflect: true, mutable: true }) open: boolean
+
+  onCloseDrawer() {
+    this.open = false
+  }
 
   render() {
-    let content = null
-    if (this.open) {
-      content = (
-        <aside>
-          <header><h1>{this.title}</h1></header>
-          <main>
-            <slot></slot>
-          </main>
-        </aside>
-      )
-    }
-    return content
+    return (
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
+        <main>
+          <slot></slot>
+        </main>
+      </aside>
+    )
   }
 }
