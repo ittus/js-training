12c509d335b9387498886e3d193b0bed1bd64514
Vu Minh Thang <vuminhthang.cm@gmail.com>
2020-01-04 10:00:01 +0900

    Create stencil component

---
+++ web-components/udemy-web-components-stenciljs/web-component-stencil/src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,21 @@
+import { h, Component, Prop } from '@stencil/core'
+
+@Component({
+  tag: 'uc-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true
+})
+export class SideDrawer {
+  @Prop({ reflect: true }) title: string
+
+  render() {
+    return (
+      <aside>
+        <header><h1>{this.title}</h1></header>
+        <main>
+          <slot></slot>
+        </main>
+      </aside>
+    )
+  }
+}
